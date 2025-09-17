c1e5ba3ae01f8843dcdbb20dbacc98883bd2e16d
angelocostanza <acostanza1107@gmail.com>
2025-09-17 09:15:13 -0400

    Finished Project

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -1,28 +1,28 @@
 package com.mycompany.csc229_211review_lab02hw;
 
+import java.util.Scanner; // import scanner so we can take inputs from user and use them in gettor and settor
+
 /**
  *
- * @author MoaathAlrajab
- */
+
+ @author MoaathAlrajab*/
 public class DriverClass {
-	
-	public static void main(String[] args) {
-		// ToDo 5: Fix the error
-		
-		// ToDo 6: Fix the constructor of Student class
-		
-		// ToDo 7: Add a toString method for Student class
-		
-		Student std1= new Student("James", 20);
-		
-		// ToDo 8: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
-		
-		System.out.println(std1);
-		
-		// ToDo 9: add comments and explain your code
-		
-		// ToDo 10: submit using a pull request.
-	}
+
+    public static void main(String[] args) {
+
+        Student std1= new Student("James", 20); // create student object w/ parameters name and age
+
+        Scanner scnr = new Scanner(System.in); // initialize the scanner so we can take inputs from user
+
+        System.out.println("Enter GPA for " + std1.getName() + ": "); // prompt the end-user for GPA
+        double gpa = scnr.nextDouble(); // store input as a double value
+
+        std1.setGpa(gpa); // use input in the settor
+
+        System.out.println(std1); // print std1 as output
+
+
+
+    }
 
 }
--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -2,15 +2,34 @@ package com.mycompany.csc229_211review_lab02hw;
 
 /**
  *
- * @author MoaathAlrajab
- */
-public class Student {
-	// ToDo 1: Make this class a child of Person
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
+
+ @author MoaathAlrajab*/
+public class Student extends Person { // Student class inherits from Person in order to become a child class of Person
+
+
+    private String address; // bring over address & gpa in order to use in gettor and settor
+    private double gpa;
+
+    public Student(String name, int age){ // bring the super constructor over from constructor that takes only two paras
+        super(name, (short) age);
+    }
+    @Override // override abstract methods from Person class
+    public String getAddress(){
+        return address;
+    }
+    @Override // override abstract methods from Person class
+    public  void setAddress(String address){
+        this.address = address;
+    }
+
+    public double getGpa(){ // gettor for gpa
+        return gpa;
+    }
+
+    public void setGpa(double gpa){ // settor for gpa
+        this.gpa = gpa;
+    }
+
+
 
 }
